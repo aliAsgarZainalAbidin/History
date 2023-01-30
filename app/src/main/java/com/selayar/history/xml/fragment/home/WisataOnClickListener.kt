41e04851b23cd91315606eeb262ded1adaa2068f
Ali <arogan929@gmai.com>
2023-01-30 16:02:17 +0800

    remove old project

--- app/src/main/java/com/selayar/history/xml/fragment/home/WisataOnClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.selayar.history.xml.fragment.home
-
-import androidx.cardview.widget.CardView
-import com.selayar.history.xml.Models.WisataSejarah
-
-interface WisataOnClickListener {
-    fun WisataOnClickListener(wisataSejarah: WisataSejarah, position : Int, root: CardView){}
-}
